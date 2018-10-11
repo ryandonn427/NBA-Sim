818f208c06e40805d2008dc6c2fe3f955f870f2f
ryandonn427 <ryandonn427@gmail.com>
2018-10-10 21:32:13 -0400

    Fixed an issue where the date was not being preserved

--- buildDatabase.java
+++ buildDatabase.java
@@ -11,69 +11,73 @@ import java.util.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class buildDatabase {
-	public  static JSONObject quarterTableValues(int date, String id, int quarter) throws Exception{
+	static int date = 0;
+	public buildDatabase(int date){
+		this.date = date;
+	}
+	public  static JSONObject quarterTableValues(String id, int quarter) throws Exception{
 		//v1/#### is the date and 00217 is the year and 00004 is the id of the game
 		//int date = 1017;
 		//int id = 1;
 		//int quarter=1;
 		String url = String.format("https://data.nba.net/prod/v1/2017%d/00217%s_pbp_%d.json", date,id,quarter);
-		System.out.println(url);
+//		System.out.println(date);
 		URL obj = new URL(url);
-		System.out.println("created the new URL object");
+	//	System.out.println("created the new URL object");
 		HttpURLConnection con  = (HttpURLConnection) obj.openConnection();
-		System.out.println("Created the connection");
+	//	System.out.println("Created the connection");
 		con.setRequestMethod("GET");
-		System.out.println("Sent the GET");
+	//	System.out.println("Sent the GET");
 		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134");
-		System.out.println("Set the user agent");
+	//	System.out.println("Set the user agent");
 		int responseCode = con.getResponseCode();
-		System.out.println("Got the response code");
+	//	System.out.println("Got the response code");
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		System.out.println("Got the BufferedReader");
+	//	System.out.println("Got the BufferedReader");
 		String inputLine;
 		StringBuffer response = new StringBuffer();
-		System.out.println("Got up to the read line step");
+	//	System.out.println("Got up to the read line step");
 		while((inputLine = in.readLine()) != null) {
 			response.append(inputLine);
 		}
 		in.close();
 		JSONObject myResponse = new JSONObject(response.toString());
-		System.out.println("quartertablevalues worked");
+		//System.out.println("quartertablevalues worked");
 		return myResponse;
 	}
-	public static JSONObject getMultipleGames(int a, int b, int c,boolean d){
-		System.out.println("Intitializing the get multiple games method");
+	public static JSONObject getMultipleGames(int b, int c,boolean d){
+		//System.out.println("Intitializing the get multiple games method");
 		try{
 			String id = Integer.toString(b);
 			while(id.length() < 5){
 				id = "0" + id;
-				System.out.println(id);
+			//	System.out.println(id);
 			}
 			
-			return quarterTableValues(a,id,c);
+			return quarterTableValues(id,c);
 		}catch (Exception e){
-			System.out.println("Didn't work, trying next day");
-			a++;
+			//System.out.println("Didn't work, trying next day");
+			date++;
 			if(d == true){
-				System.out.println(a);
-				return getMultipleGames(a,b,1,false) ;
+				//System.out.println(a);
+				return getMultipleGames(b,1,false) ;
 			}else{
 				return null;
 			}
 		}
 	}
-	public static boolean runPlaybPlay(int inputDate, int inputGame, int inputQuarter, boolean d){
+	public static boolean runPlaybPlay(int inputGame, int inputQuarter, boolean d){
 		boolean trial = false;
+		//System.out.println(inputDate);
 		try{
 			//inputDate = 1017 and inputGame/inputQuarter =1 and boolean d = true
-			JSONArray test = getMultipleGames(inputDate,inputGame,inputQuarter,d).getJSONArray("plays");	
+			JSONArray test = getMultipleGames(inputGame,inputQuarter,d).getJSONArray("plays");	
 			trial = true;
 			int quarter = inputQuarter;
 			float playTime = 0;
 			String player ="";
 			String text = "";
 			int gameID = inputGame;
-			int date = inputDate;
 			int playID = 0;
 			String team1 = null;
 			String player1  = null;
@@ -82,7 +86,7 @@ public class buildDatabase {
 			String team2 = null;
 			String action2 = null;
 			int points = 0;
-			String[] teams = new String[2];
+			
 			float[] times = {720,720};
 			for(int i= 0 ; i<test.length(); i++){
 				playID=i;
@@ -111,19 +115,14 @@ public class buildDatabase {
 				times[1] = newTime;
 				playTime = times[0]-times[1];
 				text  = (String) (testDict.get("description"));
+				//System.out.println(text);
 					if(playTime<0){
 						System.out.println("There was an error for this play \n" + text + " " + Float.toString(times[0]) + " " +  Float.toString(times[1]));
 					}
 					String patternString = "[A-Z]{3}";
 					team1 = Match(patternString, text);
 					
-					if(teams[0] == null){
-						if(teams[1] == null){
-							teams[1] = team1;
-						}else{
-							teams[0] = team1;
-						}
-					}
+					
 					
 					String patternString2 = "[A-Z]{1}[a-z]+[A-Z]*[a-z]+";
 					player = Match(patternString2,text);
@@ -144,13 +143,7 @@ public class buildDatabase {
 							action1 = "Turnover";
 							player2 = generateSteal(text);
 							action2 = "Steal";
-							if(teams[0].equals(team1)){
-								team2 = teams[1];
-							}else if(teams[1].equals(team1)){
-								team2 = teams[0];
-							}else{
-								team2 = "opposing team";
-							}
+							
 						}else{
 							player1 = player;
 							action1 = "Turnover";
@@ -196,6 +189,7 @@ public class buildDatabase {
 					
 					if(text.contains("Substitution")){
 						String [] subs = generateSub(text);
+						//System.out.println(subs[1]);
 						player1 = subs[0];
 						action1 = "Subbed out";
 						player2= subs[1];
@@ -230,7 +224,7 @@ public class buildDatabase {
 			
 			//String query = "INSERT INTO pbp(playID, gameID, date, team1, player1, action1, player2, team2, action2, points, time,quarter)  \n" + 
 			//"VALUES \n" +
-			String query = String.format("(%d, %d,%d, %s, %s, %s, %s, %s, %s, %d, %.1f, %d);", playID, gameID,inputDate, team1, player1, action1, player2, team2, action2, points, playTime, quarter);
+			String query = String.format("(%d, %d,%d, %s, %s, %s, %s, %s, %s, %d, %.1f, %d);", playID, gameID,date, team1, player1, action1, player2, team2, action2, points, playTime, quarter);
 			System.out.println(query);		
 			
 			
@@ -244,30 +238,32 @@ public class buildDatabase {
 		return trial;
 	}
 
-	public static boolean runMultScrapes(int inputDate, int inputGame,int quarter){
+	public static boolean runMultScrapes(int inputGame,int quarter){
 		boolean trial = false;
-		trial = runPlaybPlay(inputDate, inputGame, quarter,true);		
-		System.out.println(trial);
+	//	System.out.println(inputDate);
+		trial = runPlaybPlay(inputGame, quarter,true);		
+	//	System.out.println(trial);
 			if(trial == true){	
 				if(quarter<4){
 					quarter++;
-					   System.out.println("Press \"ENTER\" to continue...");
+				//	   System.out.println("Press \"ENTER\" to continue...");
    					Scanner scanner = new Scanner(System.in);
-					   scanner.nextLine();
-					
-					return runMultScrapes(inputDate,inputGame,quarter);
+				//	   scanner.nextLine();
+					//System.out.println(inputDate);
+					return runMultScrapes(inputGame,quarter);
 				}
 			}else{
 				
-				inputDate++;
-				return runMultScrapes(inputDate,inputGame, quarter);
+				//System.out.println(inputDate);
+				date++;
+				return runMultScrapes(inputGame, quarter);
 				
 			}
 		return false;
 					
 	}
-	public static boolean runMultScrapes(int inputDate, int inputGame){
-		return runMultScrapes(inputDate,inputGame,1);
+	public static boolean runMultScrapes(int inputGame){
+		return runMultScrapes(inputGame,1);
 	}
 	public static String Match(String patternString, String text){
 		Pattern pattern  = Pattern.compile(patternString);
@@ -340,7 +336,7 @@ public class buildDatabase {
 	}
 	public static String[] generateSub(String play){
 		try{
-			String pattern = "[A-Za-z]+\\sSubstitution\\sreplaced\\sby\\s[A-Za-z]+";
+			String pattern = "[A-Za-z]+\\s*[A-Za-z\\.]*\\sSubstitution\\sreplaced\\sby\\s[A-Za-z]+\\s*[A-Za-z\\.]*";
 			String[] result = new String[2];
 			String subs = Match(pattern,play);
 			pattern = "^[A-Za-z]+";
@@ -378,10 +374,11 @@ public class buildDatabase {
 	}
 	public static void main(String args[]){
 		try{
-			buildDatabase a = new buildDatabase();
-			//for(int i=0 ; i<4; i++){
-			a.runMultScrapes(1017,2);
-			//}				
+			buildDatabase a = new buildDatabase(1017);
+			for(int i=1 ; i<20; i++){
+				a.runMultScrapes(i);
+				System.out.println(i);
+			}				
 		}catch(Exception e){
 			e.printStackTrace();
 		}
